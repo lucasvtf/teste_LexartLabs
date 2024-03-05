d44270007f0ba6801381748b86c38d9c422acb28
Lucas Victor <lucassvtf@gmail.com>
2024-03-05 15:44:50 -0300

    feat: includes router user on app.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,19 @@
+import express from 'express';
+import 'express-async-errors';
 import 'reflect-metadata';
 import 'dotenv/config';
-import 'express-async-errors';
-import express from 'express';
 import cors from 'cors';
+import userRoutes from './routes/UserRoutes';
+import errorHandler from './utils/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors({ origin: '*' }));
+
+app.get('/', (req, res) => res.json({ ok: true }));
+app.use(userRoutes);
+app.use(errorHandler);
 
 export default app;
